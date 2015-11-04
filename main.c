b00e2ced0c11fdf316e0fb5eb061be5fc0a36210
Daniel Lovasko <lovasko@freebsd.org>
2015-11-04 17:32:23 +0100

    command line options

--- main.c
+++ main.c
@@ -2,25 +2,60 @@
 #include <sys/time.h>
 #include <sys/event.h>
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
 
 int
 main(int argc, char** argv)
 {
-	int kq;
 	int fd;
+	int kq;
+	int option;
 	struct kevent ke;
+	unsigned int fflags;
 
-	if (argc != 2) {
+	fflags = 0;
+
+	while ((option = getopt(argc, argv, "adehlmrw")) != -1) {
+		switch(option) {
+			case 'a': fflags |= NOTE_ATTRIB; break;
+			case 'd': fflags |= NOTE_DELETE; break;
+			case 'e': fflags |= NOTE_EXTEND; break;
+			case 'l': fflags |= NOTE_LINK;   break;
+			case 'm': fflags |= NOTE_RENAME; break;
+			case 'r': fflags |= NOTE_REVOKE; break;
+			case 'w': fflags |= NOTE_WRITE;  break;
+
+			case 'h':
+				printf("checkwait [-adelmrw] file\n");
+			return EXIT_SUCCESS;
+
+			case '?':
+				fprintf(stderr, "ERROR: invalid option '%c'.\n", optopt);	
+			return EXIT_FAILURE;
+
+			default: 
+				fprintf(stderr, "ERROR: unknown error during option parsing\n");	
+			return EXIT_FAILURE;
+		}
+	}
+
+	if (argc - optind < 1) {
 		fprintf(stderr, "ERROR: expected one argument.\n");
 		return EXIT_FAILURE;
 	}
 
+	if (fflags == 0)
+		fflags = NOTE_ATTRIB  | NOTE_DELETE
+		        | NOTE_EXTEND | NOTE_LINK
+						| NOTE_RENAME | NOTE_REVOKE
+						| NOTE_WRITE; 
+
 	errno = 0;
-	fd = open(argv[1], O_RDONLY);
+	fd = open(argv[optind], O_RDONLY);
 	if (fd < 0) {
 		perror("open");
 		return EXIT_FAILURE;
@@ -36,7 +71,7 @@ main(int argc, char** argv)
 	ke.ident = (uintptr_t)fd;
 	ke.flags = EV_ADD;
 	ke.filter = EVFILT_VNODE;
-	ke.fflags = NOTE_WRITE | NOTE_DELETE;
+	ke.fflags = fflags;
 
 	errno = 0;
 	if (kevent(kq, &ke, 1, &ke, 1, NULL) == -1) {
